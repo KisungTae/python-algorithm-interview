e3b036d1e6304eafee2ba6a54c0e1a57a4aa2622
KisungTae <kstae023@gmail.com>
2021-11-06 23:12:07 +1100

    done merge two sorted linked list

---
+++ src/com/company/linkedlist/MergeTwoSortedLinkedList.java
@@ -0,0 +1,69 @@
+package com.company.linkedlist;
+
+public class MergeTwoSortedLinkedList {
+
+    public class ListNode {
+        int                           val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode head = null;
+
+        if (l1 == null && l2 == null) {
+
+        } else if (l1 == null) {
+            head = new ListNode(l2.val);
+            l2 = l2.next;
+        } else if (l2 == null) {
+            head = new ListNode(l1.val);
+            l1 = l1.next;
+        } else if (l1.val < l2.val) {
+            head = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            head = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode itr = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                itr.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                itr.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            itr = itr.next;
+        }
+
+        while (l1 != null) {
+            itr.next = new ListNode(l1.val);
+            l1 = l1.next;
+            itr = itr.next;
+        }
+
+        while (l2 != null) {
+            itr.next = new ListNode(l2.val);
+            l2 = l2.next;
+            itr = itr.next;
+        }
+
+        return head;
+    }
+}
