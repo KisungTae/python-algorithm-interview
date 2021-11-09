c6baa878635494eddedf75e3c474ac7bf9da1e41
KisungTae <kstae023@gmail.com>
2021-11-10 08:26:41 +1100

    done swap nodes in pairs

---
+++ src/com/company/linkedlist/SwapNodesInPairs_R.java
@@ -0,0 +1,92 @@
+package com.company.linkedlist;
+
+public class SwapNodesInPairs_R {
+
+    // your solution is right but the solution from the book does not have if statement in while,
+    // review the solution from the book and re-do
+
+    public class ListNode {
+        int                        val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode itr = head;
+
+        if (head != null && head.next != null) {
+            head = head.next;
+        }
+
+        ListNode prev = null;
+        while (itr != null && itr.next != null) {
+            ListNode curr = itr;
+            ListNode next = itr.next;
+
+            curr.next = next.next;
+            next.next = curr;
+            if (prev != null) {
+                prev.next = next;
+            }
+            prev = curr;
+            itr = curr.next;
+        }
+
+        return head;
+    }
+
+}
