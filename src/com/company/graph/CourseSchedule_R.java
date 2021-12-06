46448582cd4f4ecf4153de2a36b3254621ade3b9
KisungTae <kstae023@gmail.com>
2021-12-06 21:26:21 +1100

    done course schedule

---
+++ src/com/company/graph/CourseSchedule_R.java
@@ -0,0 +1,14 @@
+package com.company.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CourseSchedule_R {
+
+    // could not solve the problem
+
+    public CourseSchedule_R() {
+        Queue<Integer> queue = new LinkedList<>();
+
+    }
+}
